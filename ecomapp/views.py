60610cc05be5e2aabab02e4c74be72b485c4df77
Deiklov <romanov408g@mail.ru>
2019-07-11 19:37:11 +0300

    19:37

--- ecomapp/views.py
+++ ecomapp/views.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.shortcuts import render
 from ecomapp.models import Category, Product
 
 def base_view(request):
+    test="testing"
     categories=Category.objects.all()
     products=Product.objects.all()
     context={
