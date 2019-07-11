f8b3c32b32404b918f487c079d8afe2ccdddff4f
Deiklov <romanov408g@mail.ru>
2019-07-11 08:56:38 +0300

    8:56 11.07 pc

--- ecomapp/admin.py
+++ ecomapp/admin.py
@@ -2,5 +2,8 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-
-# Register your models here.
+from ecomapp.models import Category,Brand,Product,TitlePrepopulated
+# admin.site.register(Category)
+admin.site.register(Brand)
+admin.site.register(Product)
+admin.site.register(Category,TitlePrepopulated)
--- ecomapp/models.py
+++ ecomapp/models.py
@@ -2,5 +2,44 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib import admin
+from django.db.models.signals import pre_save
+from django.utils.text import slugify
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(blank=True)
+
+    def __unicode__(self):
+        return self.name
+
+
+class TitlePrepopulated(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("name",)}
+
+
+class Brand(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __unicode__(self):
+        return self.name
+
+
+def image_folder(instance, filename):
+    filename = instance.slug + '.' + filename.split('.')[1]
+    return "{0}/{1}".format(instance.slug, filename)
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category)
+    brand = models.ForeignKey(Brand)
+    title = models.CharField(max_length=120)
+    slug = models.SlugField()
+    description = models.TextField()
+    image = models.ImageField(upload_to=image_folder)
+    price = models.DecimalField(max_digits=9, decimal_places=2)
+    available = models.BooleanField(default=True)
+
+    def __unicode__(self):
+        return self.title
