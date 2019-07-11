ddefe84596cd835c4f72f4cab0756b39a2ad85f8
Deiklov <romanov408g@mail.ru>
2019-07-11 17:58:11 +0300

    17:57 pc add boosttrap

--- djangoshop/urls.py
+++ djangoshop/urls.py
@@ -1,12 +1,5 @@
 """djangoshop URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
 Including another URLconf
@@ -20,7 +13,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^',include('ecomapp.urls'))
+    url(r'^',include('ecomapp.urls')),
 ]
 if settings.DEBUG:
     urlpatterns+=static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
--- ecomapp/views.py
+++ ecomapp/views.py
@@ -2,6 +2,13 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from ecomapp.models import Category, Product
 
 def base_view(request):
+    categories=Category.objects.all()
+    products=Product.objects.all()
+    context={
+        'categories': categories,
+        'products':products
+    }
     return render(request,'base.html',{})
