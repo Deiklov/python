3d19f6992510d5c4a335b2f23ad95a2ba55f7d96
Deiklov <romanov408g@mail.ru>
2019-07-15 23:45:20 +0300

    23:01

--- ecomapp/views.py
+++ ecomapp/views.py
@@ -90,7 +90,13 @@ def add_to_cart_view(request):
     product_slug = request.GET.get('product_slug')
     product = Product.objects.get(slug=product_slug)
     cart.add_to_cart(product.slug)
-    return JsonResponse({'cart_total': cart.items.count()})
+    new_cart_total = 0.00
+    for item in cart.items.all():
+        new_cart_total += float(item.item_total)
+    cart.cart_total = new_cart_total
+    cart.save()
+    return JsonResponse({'cart_total': cart.items.count(),
+                         'cart_total_price': cart.cart_total})
 
 
 def remove_from_cart_view(request):
@@ -107,7 +113,13 @@ def remove_from_cart_view(request):
     product_slug = request.GET.get('product_slug')
     product = Product.objects.get(slug=product_slug)
     cart.remove_from_cart(product.slug)
-    return JsonResponse({'cart_total': cart.items.count()})
+    new_cart_total = 0.00
+    for item in cart.items.all():
+        new_cart_total += float(item.item_total)
+    cart.cart_total = new_cart_total
+    cart.save()
+    return JsonResponse({'cart_total': cart.items.count(),
+                         'cart_total_price': cart.cart_total})
 
 
 def change_item_qty(request):
@@ -126,5 +138,10 @@ def change_item_qty(request):
     cart_item = CartItem.objects.get(id=int(item_id))
     cart_item.item_total = int(qty) * Decimal(cart_item.product.price)
     cart_item.save()
-
-    return JsonResponse({'cart_total': cart.items.count(), 'item_total': cart_item.item_total})
+    new_cart_total = 0.00
+    for item in cart.items.all():
+        new_cart_total += float(item.item_total)
+    cart.cart_total = new_cart_total
+    cart.save()
+    return JsonResponse({'cart_total': cart.items.count(), 'item_total': cart_item.item_total,
+                         'cart_total_price': cart.cart_total})
